af08aa897aa21d8490d6dec140e6b1d206b20d1c
Nikita Negodov <nn@smarthead.ru>
2023-07-20 12:11:46 +0300

    changed visibility of links on main page

--- app/page.tsx
+++ app/page.tsx
@@ -13,28 +13,25 @@ export default async function Home() {
             <h1>NEXT-13 TEST APP</h1>
             <br />
 
+            <h1>Sections</h1>
+
+            <h2>Routing</h2>
+            <Link href={'/promo'}>Promo (Layout sharing)</Link>
+            <Link href={'/products'}>
+                Products (Layout sharing, dynamic segments, suspense)
+            </Link>
+            <Link href={'/discounts'}>
+                Discounts (Statically generated dynamic segments)
+            </Link>
+
+            <br />
+            <Link href={'/clients'}>Clients (Local error boundary)</Link>
+            <Link href={'/dashboard'}>Dashboard (Parallel routing)</Link>
+            <br />
+            <br />
+
             {isAuthorized ? (
                 <>
-                    <h1>Sections</h1>
-
-                    <h2>Routing</h2>
-                    <Link href={'/promo'}>Promo (Layout sharing)</Link>
-                    <Link href={'/products'}>
-                        Products (Layout sharing, dynamic segments, suspense)
-                    </Link>
-                    <Link href={'/discounts'}>
-                        Discounts (Statically generated dynamic segments)
-                    </Link>
-
-                    <br />
-                    <Link href={'/clients'}>
-                        Clients (Local error boundary)
-                    </Link>
-                    <Link href={'/dashboard'}>
-                        Dashboard (Parallel routing)
-                    </Link>
-                    <br />
-                    <br />
                     <h2>Server & Client components</h2>
 
                     <Link href={'/contacts'}>
@@ -60,7 +57,10 @@ export default async function Home() {
                 </>
             ) : (
                 <>
-                    <p>This is a test app. Please log in to continue</p>
+                    <p>
+                        This is a test app. Part of routes are available only
+                        after logging in
+                    </p>
                 </>
             )}
         </div>
