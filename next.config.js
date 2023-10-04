6cd28fc61ebc69cea48919f2a1cf8d4343aa04c7
Nikita Negodov <nn@smarthead.ru>
2023-10-04 11:20:19 +0400

    next update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
                 port: '',
                 pathname: '/**',
             },
