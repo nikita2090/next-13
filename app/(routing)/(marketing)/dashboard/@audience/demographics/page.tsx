f96b03f34d0b6bb3a9064842328e27077c121cc4
Nikita Negodov <nn@smarthead.ru>
2023-10-05 13:48:17 +0400

    added fallbacks to parallel routing example

--- app/(routing)/(marketing)/dashboard/@audience/demographics/page.tsx
+++ app/(routing)/(marketing)/dashboard/@audience/demographics/page.tsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function Demographics() {
+    const router = useRouter();
     return (
         <div>
             <h4>Demographics</h4>
             <p>Demographics content</p>
+
+            <button onClick={() => router.back()}>back</button>
         </div>
     );
 }
